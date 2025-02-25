aa1d11939289d9a787efb4f9c3d915a31a3c14d4
unknown <nikobagus37@gmail.com>
2025-02-25 19:40:14 +0700

    fix tailwind

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Devina's Birthday</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     <script src="//unpkg.com/alpinejs" defer></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 
